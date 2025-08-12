e5034c9faebe4acdea69a4ad1bbb8a687d86e40f
Vidh singh Chauhan <vidha.vsc@gmail.com>
2025-08-12 20:38:49 +0530

    refractor: remove asyncio.run call to move to main

--- NotnotichanBot.py
+++ NotnotichanBot.py
@@ -3,7 +3,6 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import random_quotes_generator
 from handlers.gmail_handler import get_gmail_service, fetch_unread_emails, mark_as_read
 from handlers.outlook_handler import fetch_unread_emails_outlook
-import asyncio
 from dotenv import load_dotenv
 import os
 
@@ -27,9 +26,7 @@ def notichan_outlook_text():
     text = f"{emails}"
     return text
 
-async def notichan_gmail_text_sender():
+async def notichan_text_sender():
     quote = random_quotes_generator.get_random_quotes()
     await app.bot.send_message(chat_id = id, text = f"gm, {quote},\n {notichan_gmail_text()}" )
     await app.bot.send_message(chat_id = id, text = f"{notichan_outlook_text()}")
-
-asyncio.run(notichan_gmail_text_sender())
