462e97113773cc75cac7dffe5a3f5de9309889bf
Vidh singh Chauhan <vidha.vsc@gmail.com>
2025-08-12 19:59:36 +0530

    fix(functions): correct gmail fetch, quote inside bot func, add asyncio.run

--- NotnotichanBot.py
+++ NotnotichanBot.py
@@ -3,6 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import random_quotes_generator
 from handlers.gmail_handler import get_gmail_service, fetch_unread_emails, mark_as_read
 from handlers.outlook_handler import fetch_unread_emails_outlook
+import asyncio
 from dotenv import load_dotenv
 import os
 
@@ -13,11 +14,11 @@ id = os.getenv("NOTNOTICHAN_ID")
 
 app = ApplicationBuilder().token(token).build()
 
-quote = random_quotes_generator.get_random_quotes()
+
 
 def notichan_gmail_text():
     service = get_gmail_service()
-    emails = fetch_unread_emails()
+    emails = fetch_unread_emails(service)
     text = f"{emails}"
     return text
 
@@ -27,5 +28,8 @@ def notichan_outlook_text():
     return text
 
 async def notichan_gmail_text_sender():
+    quote = random_quotes_generator.get_random_quotes()
     await app.bot.send_message(chat_id = id, text = f"gm, {quote},\n {notichan_gmail_text()}" )
     await app.bot.send_message(chat_id = id, text = f"{notichan_outlook_text()}")
+
+asyncio.run(notichan_gmail_text_sender())
