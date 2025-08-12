ceeb642218dee6c5fe9b0008eeebfb4f272b6ec2
Vidh singh Chauhan <vidha.vsc@gmail.com>
2025-08-12 09:51:59 +0530

    feat: text & text sender

--- NotnotichanBot.py
+++ NotnotichanBot.py
@@ -1,5 +1,8 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import random_quotes_generator
+from handlers.gmail_handler import get_gmail_service, fetch_unread_emails, mark_as_read
+from handlers.outlook_handler import fetch_unread_emails_outlook
 from dotenv import load_dotenv
 import os
 
@@ -10,5 +13,19 @@ id = os.getenv("NOTNOTICHAN_ID")
 
 app = ApplicationBuilder().token(token).build()
 
-async def notichan_text_sender():
-    await app.bot.send_message(chat_id = id, text = "hi")
+quote = random_quotes_generator.get_random_quotes()
+
+def notichan_gmail_text():
+    service = get_gmail_service()
+    emails = fetch_unread_emails()
+    text = f"{emails}"
+    return text
+
+def notichan_outlook_text():
+    emails = fetch_unread_emails_outlook()
+    text = f"{emails}"
+    return text
+
+async def notichan_gmail_text_sender():
+    await app.bot.send_message(chat_id = id, text = f"gm, {quote},\n {notichan_gmail_text()}" )
+    await app.bot.send_message(chat_id = id, text = f"{notichan_outlook_text()}")
